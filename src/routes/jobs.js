e8d2ac4f6ef34e750842e8d8a69dc42a9d106764
amubasshir <lastmubasshir@gmail.com>
2025-04-23 00:06:19 +0600

    Fix: apply jobs, job details show, schedule modal added in job post card

--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -16,19 +16,23 @@ const router = express.Router();
 
 router.get('/',getAllJobs);
 // Create a new job
-router.post('/', protect, hasRole('recruiter'), createJob);
+// router.post('/', protect, hasRole('recruiter'), createJob);
+router.post('/', createJob);
 
 // router.post('/generate-job-post', protect, hasRole('recruiter'), createJobPost);
 router.post('/generate-job-post', createJobPost);
 
 // Update a job
-router.put('/:id', protect, hasRole('recruiter'), updateJob);
+// router.put('/:id', protect, hasRole('recruiter'), updateJob);
+router.put('/:id', updateJob);
 
 // Delete a job
-router.delete('/:id', protect, hasRole('recruiter'), deleteJob);
+// router.delete('/:id', protect, hasRole('recruiter'), deleteJob);
+router.delete('/:id', deleteJob);
 
 // Get all jobs with pagination and filters
-router.get('/my-jobs',protect, hasRole('recruiter'),getAllMyJobs);
+// router.get('/my-jobs',protect, hasRole('recruiter'),getAllMyJobs);
+router.get('/my-jobs', getAllMyJobs);
 
 
 // Get a single job by ID
