0c5f9630fac38b1a53bbe7eedd2e1432df47b111
amubasshir <lastmubasshir@gmail.com>
2025-07-10 03:03:25 +0600

    updated backend

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -181,7 +181,7 @@ const createToken = (user) => {
   return jwt.sign(
     { id: user._id, email: user.email, role: user.role },
     process.env.JWT_SECRET,
-    { expiresIn: '1d' }
+    { expiresIn: '30d' }
   );
 };
 
